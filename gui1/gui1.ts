afb5e3f4da5ee1c57c2e0cb488dafb4f4c1d3f87
gheja <gheja@w-7.git.stdio.hu>
2020-08-22 23:40:02 +0200

    gui1: added current WIP version

--- gui1/gui1.ts
+++ gui1/gui1.ts
@@ -1,5 +1,52 @@
 "use strict";
 
+function getNextZIndex()
+{
+    if (!getNextZIndex.n)
+    {
+        getNextZIndex.n = 9000;
+    }
+    getNextZIndex.n++;
+    return getNextZIndex.n + "";
+}
+
+function windowMouseDown(event: MouseEvent)
+{
+    // console.log("down");
+    (event.currentTarget as HTMLElement).dataset.selected = "1";
+    (event.currentTarget as HTMLElement).style.zIndex = getNextZIndex();
+}
+
+function windowMouseUp()
+{
+    let a: HTMLElement;
+
+    // console.log("up");
+
+    for (a of document.querySelectorAll<HTMLElement>(".window"))
+    {
+        a.dataset.selected = "0";
+    }
+}
+
+function windowUpdate(dx, dy)
+{
+    let a: HTMLElement;
+    let b: DOMRect;
+
+    // console.log(dx, dy);
+
+    for (a of document.querySelectorAll<HTMLElement>(".window"))
+    {
+        if (a.dataset.selected == "1")
+        {
+            b = a.getBoundingClientRect();
+            a.style.top = b.top + dy + "px";
+            a.style.left = b.left + dx + "px";
+        }
+    }
+}
+
 function createWindow()
 {
     let win: HTMLDivElement;
@@ -7,6 +54,8 @@ function createWindow()
 
     win = document.createElement("div");
     win.className = "window";
+    win.addEventListener("mousedown", windowMouseDown.bind(null));
+    win.addEventListener("mouseup", windowMouseUp.bind(null));
 
     a = document.createElement("div");
     a.className = "title";
@@ -20,3 +69,20 @@ function createWindow()
 
     document.body.appendChild(win);
 }
+
+let _mouseX = 0;
+let _mouseY = 0;
+
+function onMouseMove(event: MouseEvent)
+{
+    windowUpdate(event.screenX - _mouseX, event.screenY - _mouseY)
+    _mouseX = event.screenX;
+    _mouseY = event.screenY;
+}
+
+function init()
+{
+    window.addEventListener("mousemove", onMouseMove);
+}
+
+window.addEventListener("load", init);
