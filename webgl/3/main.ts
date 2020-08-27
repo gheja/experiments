4adb4ada14f10de9b10b37b06465c67a75fe1fc1
gheja <gheja@w-7.git.stdio.hu>
2020-08-27 03:46:06 +0200

    webgl: added current WIP version

--- webgl/3/main.ts
+++ webgl/3/main.ts
@@ -10,9 +10,9 @@ let cameraMatrix;
 let model;
 let mvp;
 let inverseTranspose;
-let indicesLength;
 
 let shapes;
+let ticks = 0;
 
 function tick()
 {
@@ -23,33 +23,27 @@ function tick()
     let mvpMatrix;
     let inverseTransposeMatrix;
 
-    shapes[0].ry += 0.01;
-    shapes[1].ry -= 0.02;
+    ticks++;
+
+    shapes[0].ry += 0.013;
+    shapes[1].ry -= 0.017;
+    shapes[2].ry -= 0.022;
+    shapes[0].x += Math.cos(ticks * 0.05) * 0.05;
+    shapes[1].y += Math.cos(ticks * 0.03) * 0.05;
+    shapes[2].z += Math.cos(ticks * 0.03) * 0.05;
 
     gl.useProgram(program);
-/*
-    function buffer(gl, data, program, attribute, size, type)
-    {
-        gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-        gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-        let a = gl.getAttribLocation(program, attribute);
-        gl.vertexAttribPointer(a, size, type, false, 0, 0);
-        gl.enableVertexAttribArray(a);
-*/
-    let a_p = gl.getAttribLocation(program, "p");
-    let a_n = gl.getAttribLocation(program, "n");
-    let a_c = gl.getAttribLocation(program, "c");
 
-    let b_p = gl.createBuffer();
-    let b_n = gl.createBuffer();
-    let b_i = gl.createBuffer();
+    let a_position = gl.getAttribLocation(program, "p");
+    let a_normal = gl.getAttribLocation(program, "n");
+    let a_color = gl.getAttribLocation(program, "c");
 
     // Render
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     for (shape of shapes)
     {
         modelMatrix = identity();
-        modelMatrix = transform(modelMatrix, { ry: shape.ry });
+        modelMatrix = transform(modelMatrix, { ry: shape.ry, x: shape.x, y: shape.y, z: shape.z });
         gl.uniformMatrix4fv(model, false, modelMatrix);
 
         // Set the cube's mvp matrix (camera x model)
@@ -60,50 +54,45 @@ function tick()
         inverseTransposeMatrix = transpose(inverse(modelMatrix));
         gl.uniformMatrix4fv(inverseTranspose, false, inverseTransposeMatrix);
 
-        // gl.bindVertexArray(shape.vao);
-        gl.drawElements(gl.TRIANGLES, indicesLength, gl.UNSIGNED_SHORT, 0);
+        gl.bindBuffer(gl.ARRAY_BUFFER, shape.b_p);
+        gl.vertexAttribPointer(a_position, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(a_position);
 
-        // Count vertices
-        indicesLength = shape.indices.length;
+        gl.bindBuffer(gl.ARRAY_BUFFER, shape.b_n);
+        gl.vertexAttribPointer(a_normal, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(a_normal);
 
-        // Set position, normal buffers
-        // buffer(gl, shape.vertices, program, 'p', 3, gl.FLOAT);
-        // buffer(gl, shape.normals, program, 'n', 3, gl.FLOAT);
+        gl.vertexAttrib3f(a_color, shape.color[0], shape.color[1], shape.color[2]);
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, b_p);
-        gl.bufferData(gl.ARRAY_BUFFER, shape.vertices, gl.STATIC_DRAW);
-        gl.vertexAttribPointer(a_p, 3, gl.FLOAT, false, 0, 0);
-
-        gl.bindBuffer(gl.ARRAY_BUFFER, b_n);
-        gl.bufferData(gl.ARRAY_BUFFER, shape.normals, gl.STATIC_DRAW);
-        gl.vertexAttribPointer(a_n, 3, gl.FLOAT, false, 0, 0);
-
-        // Set indices
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, b_i);
-        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, shape.indices, gl.STATIC_DRAW);
-
-        // Set cube color
-        gl.vertexAttrib3f(a_c, shape.color[0], shape.color[1], shape.color[2]);
-
-        gl.enableVertexAttribArray(a_p);
-        gl.enableVertexAttribArray(a_n);
-
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, shape.b_i);
+        gl.drawElements(gl.TRIANGLES, shape.indices_length, gl.UNSIGNED_SHORT, 0);
     }
-
 }
 
 function getShape(cr, cg, cb)
 {
+    function buffer(x, type)
+    {
+        let a;
+
+        a = gl.createBuffer();
+        gl.bindBuffer(type, a);
+        gl.bufferData(type, x, gl.STATIC_DRAW);
+
+        return a;
+    }
+
     // Initialize a cube
     let vertices, normals, indices;
     [vertices, normals, indices] = cube();
 
     return {
-        vertices: vertices,
-        normals: normals,
         indices: indices,
         color: [ cr, cg, cb ],
-        vao: 0,
+        b_p: buffer(vertices, gl.ARRAY_BUFFER),
+        b_n: buffer(normals, gl.ARRAY_BUFFER),
+        b_i: buffer(indices, gl.ELEMENT_ARRAY_BUFFER),
+        indices_length: indices.length,
         ry: 0,
         x: 0,
         y: 0,
@@ -124,7 +113,7 @@ function init() {
 
     // Set the camera
     cameraMatrix = perspective({ fov: 0.5, aspect: 1, near: 1, far: 100 });
-    cameraMatrix = transform(cameraMatrix, { z: -3.5, rx: .5, ry: -.5 });
+    cameraMatrix = transform(cameraMatrix, { z: -5 });
 
     // Set the point light color and position
     let lightColor = gl.getUniformLocation(program, 'lc');
@@ -144,7 +133,8 @@ function init() {
 
     shapes = [
         getShape(1, 0, 0),
-        getShape(0, 1, 0)
+        getShape(0, 1, 0),
+        getShape(0, 0, 1)
     ];
 
     // Loop
