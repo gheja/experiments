e0e84f13e48b6e131624f8ac6851836c59d142e7
gheja <gheja@w-7.git.stdio.hu>
2020-08-29 23:04:52 +0200

    webgl: added 4th version, it now support coloring per vertex

---
+++ webgl/4/main.ts
@@ -0,0 +1,180 @@
+/*
+    This code is based heavily on Xem's WebGL guide.
+    https://xem.github.io/articles/webgl-guide.html
+
+    Thanks Xem!
+*/
+let gl: WebGLRenderingContext;
+let program: WebGLProgram;
+let cameraMatrix: Float32Array;
+
+let shapes;
+let objects;
+let ticks: number = 0;
+let colorPalette: Array<Array<number>> = [
+    [ 255, 0, 0, 255 ],
+    [ 255, 255, 0, 255 ],
+    [ 0, 255, 0, 255 ],
+    [ 0, 255, 255, 255 ],
+    [ 0, 0, 255, 255 ],
+    [ 255, 0, 255, 255 ],
+    [ 255, 255, 255, 255 ],
+];
+
+function render()
+{
+    let obj;
+    let modelMatrix: Float32Array;
+    let mvpMatrix: Float32Array;
+    let inverseTransposeMatrix: Float32Array;
+    let a_position: GLint;
+    let a_normal: GLint;
+    let a_color: GLint;
+    let u_model: WebGLUniformLocation;
+    let u_mvp: WebGLUniformLocation;
+    let u_inverseTranspose: WebGLUniformLocation;
+
+    gl.useProgram(program);
+
+    a_position = gl.getAttribLocation(program, "p");
+    a_normal = gl.getAttribLocation(program, "n");
+    a_color = gl.getAttribLocation(program, "c");
+
+    u_model = gl.getUniformLocation(program, 'o');
+    u_mvp = gl.getUniformLocation(program, 'm');
+    u_inverseTranspose = gl.getUniformLocation(program, 'i');
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    for (obj of objects)
+    {
+        modelMatrix = identity();
+        // obj has x, y, z, rx, ry, rz just like transform() requires in options
+        modelMatrix = transform(modelMatrix, obj);
+        gl.uniformMatrix4fv(u_model, false, modelMatrix);
+
+        mvpMatrix = multMat4Mat4(cameraMatrix, modelMatrix);
+        gl.uniformMatrix4fv(u_mvp, false, mvpMatrix);
+
+        inverseTransposeMatrix = transpose(inverse(modelMatrix));
+        gl.uniformMatrix4fv(u_inverseTranspose, false, inverseTransposeMatrix);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, obj.shape.b_p);
+        gl.vertexAttribPointer(a_position, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(a_position);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, obj.shape.b_n);
+        gl.vertexAttribPointer(a_normal, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(a_normal);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, obj.shape.b_c);
+        gl.vertexAttribPointer(a_color, 4, gl.UNSIGNED_BYTE, true, 0, 0);
+        gl.enableVertexAttribArray(a_color);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, obj.shape.b_i);
+        gl.drawElements(gl.TRIANGLES, obj.shape.indices_length, gl.UNSIGNED_SHORT, 0);
+    }
+}
+
+function tick()
+{
+    ticks++;
+
+    objects[0].ry += 0.013;
+    objects[1].ry -= 0.038;
+    objects[2].ry -= 0.022;
+    objects[0].x += Math.cos(ticks * 0.05) * 0.05;
+    objects[1].y += Math.cos(ticks * 0.03) * 0.05;
+    objects[2].z += Math.cos(ticks * 0.03) * 0.05;
+
+    render();
+}
+
+function getShape0()
+{
+    function buffer(x, type)
+    {
+        let a;
+
+        a = gl.createBuffer();
+        gl.bindBuffer(type, a);
+        gl.bufferData(type, x, gl.STATIC_DRAW);
+
+        return a;
+    }
+
+    // Initialize a cube
+    let vertices: Float32Array, indices: Uint16Array, colors: Uint8Array, colors2: Uint8Array;
+    let i;
+
+    [ vertices, indices, colors ] = cube2();
+
+    colors2 = new Uint8Array(colors.length * 4);
+
+    for (i in colors)
+    {
+        console.log(i, colors[i]);
+        colors2.set(colorPalette[colors[i]], i * 4);
+    }
+
+    return {
+        b_p: buffer(vertices, gl.ARRAY_BUFFER),
+        b_i: buffer(indices, gl.ELEMENT_ARRAY_BUFFER),
+        b_n: buffer(calculateNormals(vertices, indices), gl.ARRAY_BUFFER),
+        b_c: buffer(colors2, gl.ARRAY_BUFFER),
+        indices_length: indices.length
+    };
+}
+
+function createObject(shape, cr, cg, cb)
+{
+    return {
+        shape: shape,
+        ry: 0,
+        x: 0,
+        y: 0,
+        z: 0
+    };
+}
+
+function init() {
+    let vshader: string;
+    let fshader: string;
+
+    gl = (document.getElementById("canvas") as HTMLCanvasElement).getContext('webgl');
+    vshader = `attribute vec4 p,c,n;uniform mat4 m,o,i;varying vec4 vc;varying vec3 vn,vp;void main(){gl_Position=m*p;vp=vec3(o*p);vn=normalize(vec3(i*n));vc=c;}`;
+    fshader = `precision mediump float;uniform vec3 lc,lp,al;varying vec3 vn,vp;varying vec4 vc;void main(){vec3 l=normalize(lp-vp);float n=max(dot(l,vn),0.0);vec3 q=lc*vc.rgb*n+al*vc.rgb;gl_FragColor=vec4(q,1.0);}`;
+    program = compile(gl, vshader, fshader);
+
+    gl.clearColor(0, 0, 0.2, 1);
+    gl.enable(gl.DEPTH_TEST);
+
+    // Set the camera
+    cameraMatrix = perspective({ fov: 0.5, aspect: 1, near: 1, far: 100 });
+    cameraMatrix = transform(cameraMatrix, { z: -5 });
+
+    // Set the point light color and position
+    let lightColor = gl.getUniformLocation(program, 'lc');
+    gl.uniform3f(lightColor, 1, 1, 1);
+
+    let lightPosition = gl.getUniformLocation(program, 'lp');
+    gl.uniform3f(lightPosition, 1.5, 1.5, 3);
+
+    // Set the ambient light color
+    let ambientLight = gl.getUniformLocation(program, 'al');
+    gl.uniform3f(ambientLight, 0.1, 0.1, 0.1);
+
+    shapes = [
+        getShape0()
+    ]
+
+    objects = [
+        createObject(shapes[0],0.8, 0.1, 0),
+        createObject(shapes[0],0, 0.8, 0.2),
+        createObject(shapes[0],0, 0.3, 0.8)
+    ];
+
+    window.setInterval(tick, 1000/60);
+}
+
+window.addEventListener("load", init);
---
+++ webgl/4/webgl.ts
@@ -0,0 +1,331 @@
+/*
+    This code is based heavily on Xem's WebGL guide.
+    https://xem.github.io/articles/webgl-guide.html
+
+    Thanks Xem!
+*/
+
+// Compile a WebGL program from a vertex shader and a fragment shader
+function compile(gl, vshader, fshader): WebGLProgram
+{
+    let vs = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vs, vshader);
+    gl.compileShader(vs);
+
+    let fs = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fs, fshader);
+    gl.compileShader(fs);
+
+    let program = gl.createProgram();
+    gl.attachShader(program, vs);
+    gl.attachShader(program, fs);
+    gl.linkProgram(program);
+    gl.useProgram(program);
+
+    console.log('vertex shader:', gl.getShaderInfoLog(vs) || 'OK');
+    console.log('fragment shader:', gl.getShaderInfoLog(fs) || 'OK');
+    console.log('program:', gl.getProgramInfoLog(program) || 'OK');
+
+    return program;
+}
+
+///// matrix
+// Create an identity mat4
+function identity()
+{
+    return new Float32Array([
+        1, 0, 0, 0,
+        0, 1, 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    ]);
+}
+
+// Compute the multiplication of two mat4
+function multMat4Mat4(a, b)
+{
+    let i, ai0, ai1, ai2, ai3;
+    let c = new Float32Array(16);
+    for (i = 0; i < 4; i++) {
+        ai0 = a[i];
+        ai1 = a[i+4];
+        ai2 = a[i+8];
+        ai3 = a[i+12];
+        c[i]    = ai0 * b[0]  + ai1 * b[1]  + ai2 * b[2]  + ai3 * b[3];
+        c[i+4]  = ai0 * b[4]  + ai1 * b[5]  + ai2 * b[6]  + ai3 * b[7];
+        c[i+8]  = ai0 * b[8]  + ai1 * b[9]  + ai2 * b[10] + ai3 * b[11];
+        c[i+12] = ai0 * b[12] + ai1 * b[13] + ai2 * b[14] + ai3 * b[15];
+    }
+    return c;
+}
+
+// Create a perspective matrix
+// options: fov, aspect, near, far
+function perspective(options)
+{
+    let fov = options.fov || 1.5;
+    let aspect = options.ratio || 1; // canvas.width / canvas.height
+    let near = options.near || 0.01; // can't be 0
+    let far = options.far || 100;
+    let f = 1 / Math.tan(fov);
+    let nf = 1 / (near - far);
+    return new Float32Array([
+        f / aspect, 0, 0, 0,
+        0, f, 0, 0,
+        0, 0, (far + near) * nf, -1,
+        0, 0, (2 * near * far) * nf, 0
+    ]);
+}
+
+// Transform a mat4
+// options: x/y/z (translate), rx/ry/rz (rotate), sx/sy/sz (scale)
+function transform(mat, options)
+{
+
+    let out = new Float32Array(mat);
+
+    let x = options.x ?? 0;
+    let y = options.y ?? 0;
+    let z = options.z ?? 0;
+
+    let sx = options.sx ?? 1;
+    let sy = options.sy ?? 1;
+    let sz = options.sz ?? 1;
+
+    let rx = options.rx;
+    let ry = options.ry;
+    let rz = options.rz;
+
+    // translate
+    if(x || y || z){
+        out[12] += out[0] * x + out[4] * y + out[8]  * z;
+        out[13] += out[1] * x + out[5] * y + out[9]  * z;
+        out[14] += out[2] * x + out[6] * y + out[10] * z;
+        out[15] += out[3] * x + out[7] * y + out[11] * z;
+    }
+
+    // Rotate
+    if(rx) out.set(multMat4Mat4(out, new Float32Array([1, 0, 0, 0, 0, Math.cos(rx), Math.sin(rx), 0, 0, -Math.sin(rx), Math.cos(rx), 0, 0, 0, 0, 1])));
+    if(ry) out.set(multMat4Mat4(out, new Float32Array([Math.cos(ry), 0, -Math.sin(ry), 0, 0, 1, 0, 0, Math.sin(ry), 0, Math.cos(ry), 0, 0, 0, 0, 1])));
+    if(rz) out.set(multMat4Mat4(out, new Float32Array([Math.cos(rz), Math.sin(rz), 0, 0, -Math.sin(rz), Math.cos(rz), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1])));
+
+    // Scale
+    if(sx !== 1){
+        out[0] *= sx;
+        out[1] *= sx;
+        out[2] *= sx;
+        out[3] *= sx;
+    }
+    if(sy !== 1){
+        out[4] *= sy;
+        out[5] *= sy;
+        out[6] *= sy;
+        out[7] *= sy;
+    }
+    if(sz !== 1){
+        out[8] *= sz;
+        out[9] *= sz;
+        out[10] *= sz;
+        out[11] *= sz;
+    }
+
+    return out;
+}
+
+
+// Get the transposed of a mat4
+function transpose(m)
+{
+    return new Float32Array([
+        m[0], m[4], m[8],  m[12],
+        m[1], m[5], m[9],  m[13],
+        m[2], m[6], m[10], m[14],
+        m[3], m[7], m[11], m[15]
+    ]);
+}
+
+// Get the inverse of a mat4
+// The mat4 is not modified, a new mat4 is returned
+function inverse(m)
+{
+    let inv, det, i;
+
+    inv = new Float32Array([
+        m[5]*m[10]*m[15]  - m[5]*m[11]*m[14] - m[9]*m[6]*m[15] + m[9]*m[7]*m[14] + m[13]*m[6]*m[11] - m[13]*m[7]*m[10],
+        -m[1]*m[10]*m[15] + m[1]*m[11]*m[14] + m[9]*m[2]*m[15] - m[9]*m[3]*m[14] - m[13]*m[2]*m[11] + m[13]*m[3]*m[10],
+        m[1]*m[6]*m[15]   - m[1]*m[7]*m[14]  - m[5]*m[2]*m[15] + m[5]*m[3]*m[14] + m[13]*m[2]*m[7]  - m[13]*m[3]*m[6],
+        -m[1]*m[6]*m[11]  + m[1]*m[7]*m[10]  + m[5]*m[2]*m[11] - m[5]*m[3]*m[10] - m[9]*m[2]*m[7]   + m[9]*m[3]*m[6],
+        -m[4]*m[10]*m[15] + m[4]*m[11]*m[14] + m[8]*m[6]*m[15] - m[8]*m[7]*m[14] - m[12]*m[6]*m[11] + m[12]*m[7]*m[10],
+        m[0]*m[10]*m[15]  - m[0]*m[11]*m[14] - m[8]*m[2]*m[15] + m[8]*m[3]*m[14] + m[12]*m[2]*m[11] - m[12]*m[3]*m[10],
+        -m[0]*m[6]*m[15]  + m[0]*m[7]*m[14]  + m[4]*m[2]*m[15] - m[4]*m[3]*m[14] - m[12]*m[2]*m[7]  + m[12]*m[3]*m[6],
+        m[0]*m[6]*m[11]   - m[0]*m[7]*m[10]  - m[4]*m[2]*m[11] + m[4]*m[3]*m[10] + m[8]*m[2]*m[7]   - m[8]*m[3]*m[6],
+        m[4]*m[9]*m[15]   - m[4]*m[11]*m[13] - m[8]*m[5]*m[15] + m[8]*m[7]*m[13] + m[12]*m[5]*m[11] - m[12]*m[7]*m[9],
+        -m[0]*m[9]*m[15]  + m[0]*m[11]*m[13] + m[8]*m[1]*m[15] - m[8]*m[3]*m[13] - m[12]*m[1]*m[11] + m[12]*m[3]*m[9],
+        m[0]*m[5]*m[15]   - m[0]*m[7]*m[13]  - m[4]*m[1]*m[15] + m[4]*m[3]*m[13] + m[12]*m[1]*m[7]  - m[12]*m[3]*m[5],
+        -m[0]*m[5]*m[11]  + m[0]*m[7]*m[9]   + m[4]*m[1]*m[11] - m[4]*m[3]*m[9]  - m[8]*m[1]*m[7]   + m[8]*m[3]*m[5],
+        -m[4]*m[9]*m[14]  + m[4]*m[10]*m[13] + m[8]*m[5]*m[14] - m[8]*m[6]*m[13] - m[12]*m[5]*m[10] + m[12]*m[6]*m[9],
+        m[0]*m[9]*m[14]   - m[0]*m[10]*m[13] - m[8]*m[1]*m[14] + m[8]*m[2]*m[13] + m[12]*m[1]*m[10] - m[12]*m[2]*m[9],
+        -m[0]*m[5]*m[14]  + m[0]*m[6]*m[13]  + m[4]*m[1]*m[14] - m[4]*m[2]*m[13] - m[12]*m[1]*m[6]  + m[12]*m[2]*m[5],
+        m[0]*m[5]*m[10]   - m[0]*m[6]*m[9]   - m[4]*m[1]*m[10] + m[4]*m[2]*m[9]  + m[8]*m[1]*m[6]   - m[8]*m[2]*m[5]
+    ]);
+
+    det = m[0]*inv[0] + m[1]*inv[4] + m[2]*inv[8] + m[3]*inv[12];
+
+    if(!det) return m;
+
+    det = 1 / det;
+
+    for(i = 0; i < 16; i++) {
+        inv[i] *= det;
+    }
+    return inv;
+}
+
+function calculateNormals(vertices: Float32Array, indices: Uint16Array): Float32Array
+{
+    let normals: Float32Array;
+    let i: number;
+    let ia: number, ib: number, ic: number;
+    let va, vb, vc;
+    let b;
+    let edgeAB, edgeAC;
+
+    function minus(a: Float32Array, b: Float32Array): Float32Array
+    {
+        return new Float32Array([ a[0] - b[0], a[1] - b[1], a[2] - b[2] ]);
+    }
+
+    function cross(a: Float32Array, b: Float32Array): Float32Array
+    {
+/*
+        cx = aybz − azby
+        cy = azbx − axbz
+        cz = axby − aybx
+*/
+        return new Float32Array([ a[1] * b[2] - a[2] * b[1], a[2] * b[0] - a[0] * b[2], a[0] * b[1] - a[1] * b[0] ]);
+    }
+
+    function add(a: Float32Array, i, b: Float32Array)
+    {
+        a[i] += b[0];
+        a[i+1] += b[1];
+        a[i+2] += b[2];
+    }
+
+    normals = new Float32Array(vertices.length);
+
+    for (i=0; i<indices.length; i+=3)
+    {
+        ia = indices[i] * 3;
+        ib = indices[i+1] * 3;
+        ic = indices[i+2] * 3;
+        va = [ vertices[ia], vertices[ia + 1], vertices[ia + 2] ];
+        vb = [ vertices[ib], vertices[ib + 1], vertices[ib + 2] ];
+        vc = [ vertices[ic], vertices[ic + 1], vertices[ic + 2] ];
+        edgeAB = minus(vb, va);
+        edgeAC = minus(vc, va);
+        b = cross(edgeAB, edgeAC);
+        add(normals, ia, b);
+        add(normals, ib, b);
+        add(normals, ic, b);
+    }
+
+    for (i=0; i<normals.length; i+=3)
+    {
+        b = Math.sqrt(normals[i]**2 + normals[i+1]**2 + normals[i+2]**2);
+
+        if (b != 0)
+        {
+            normals[i] /= b;
+            normals[i+1] /= b;
+            normals[i+2] /= b;
+        }
+    }
+
+    return normals;
+}
+
+///// shapes
+// Declare a cube (2x2x2)
+// Returns [vertices (Float32Array), normals (Float32Array), indices (Uint16Array)]
+//
+//    v6----- v5
+//   /|      /|
+//  v1------v0|
+//  | |     | |
+//  | |v7---|-|v4
+//  |/      |/
+//  v2------v3
+function cube2(): [ Float32Array, Uint16Array, Uint8Array ]
+{
+    let vertices = new Float32Array([
+        1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0, // front
+        1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0, // right
+        1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0, // up
+        -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0, // left
+        -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0, // down
+        1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0  // back
+    ]);
+
+    let indices = new Uint16Array([
+        0, 1, 2,   0, 2, 3,  // front
+        4, 5, 6,   4, 6, 7,  // right
+        8, 9, 10,  8, 10,11, // up
+        12,13,14,  12,14,15, // left
+        16,17,18,  16,18,19, // down
+        20,21,22,  20,22,23  // back
+    ]);
+
+    let colors = new Uint8Array([
+        0, 0, 0, 0,
+        1, 1, 1, 1,
+        2, 2, 2, 2,
+        3, 3, 3, 3,
+        4, 4, 4, 4,
+        5, 5, 5, 5
+    ]);
+
+    return [ vertices, indices, colors ];
+}
+
+function cube3(): [ Float32Array, Uint16Array, Uint8Array ]
+{
+//    v6----- v5
+//   /|      /|
+//  v1------v0|
+//  | |     | |
+//  | |v7---|-|v4
+//  |/      |/
+//  v2------v3
+    let vertices = new Float32Array([
+        1, 1, 1,   -1, 1, 1,   -1,-1, 1,   1,-1, 1, // front
+        1, -1, -1,  1, 1, -1,  -1, 1, -1, -1, -1, -1 // back
+    ]);
+
+    let indices = new Uint16Array([
+        0, 1, 2,  0, 2, 3,
+        5, 0, 3,  5, 3, 4,
+        6, 5, 4,  6, 4, 7,
+        6, 7, 2,  6, 2, 1,
+        3, 2, 7,  3, 7, 4,
+        5, 6, 1,  5, 1, 0
+    ]);
+
+    let colors = new Uint8Array([
+        0, 1, 2, 3,
+        2, 3, 4, 5,
+    ]);
+
+    return [ vertices, indices, colors ];
+}
+
+// Convert deg in radians
+function deg2rad(angle)
+{
+    return Math.PI * angle / 180;
+}
+
+function createShape()
+{
+
+}
