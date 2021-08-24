164ee5c1a0f4044611210e49feaaf2adfee725e8
Gabor Heja <gheja@u2560p.git.stdio.hu>
2021-08-24 17:34:57 +0200

    Added _settings.drawOriginal

--- fractal/1/main.js
+++ fractal/1/main.js
@@ -7,7 +7,7 @@ let _width = 400;
 let _height = 400;
 let _t;
 let _gui;
-let _settings = { "drawBoxes": false, "alpha": 0.9 };
+let _settings = { "drawBoxes": false, "alpha": 0.9, "drawOriginal": true };
 
 let _operations = [ "normal", "multiply", "screen", "overlay", "darken", "lighten", "color-dodge", "color-burn", "hard-light", "soft-light", "difference", "exclusion", "hue", "saturation", "color", "luminosity" ];
 let operation;
@@ -55,6 +55,7 @@ function init()
 	_gui = new dat.GUI();
 	_gui.add(_settings, "drawBoxes");
 	_gui.add(_settings, "alpha", 0.0, 1.0);
+	_gui.add(_settings, "drawOriginal");
 	
 	_t = 0;
 	
@@ -108,9 +109,13 @@ function draw()
 	
 	_ctx.clearRect(0, 0, _width, _height);
 	_ctx.drawImage(_canvas2, 0, 0);
-	_ctx.fillStyle = "#c93c";
-	_ctx.globalCompositeOperation = operation;
-	_ctx.fillRect(100 + Math.sin(_t) * 100, 100 + Math.sin(_t*1.4) * 100, 200, 200); 
+	
+	if (_settings.drawOriginal)
+	{
+		_ctx.fillStyle = "#c93c";
+		_ctx.globalCompositeOperation = operation;
+		_ctx.fillRect(100 + Math.sin(_t) * 100, 100 + Math.sin(_t*1.4) * 100, 200, 200);
+	}
 	
 	// overlay
 	_ctx3.clearRect(0, 0, _width, _height);
